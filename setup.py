592da7bc600f5bb2432c3366632e191e0dcd13fa
Vladimir Berezenko <qmaster.mir@gmail.com>
2015-02-25 00:03:34 +0300

    Исправлен сетап скрипт

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import os
 setup(name='pyZXTools',
       version='1.0',
       description='ZX-Spectrum file formats tools',
-      scripts=['bin/tap.py', 'bin/trdos.py', 'bin/validate_local_iface.py'],
+      scripts=['bin/tap.py', 'bin/trdos.py'],
       packages=['pyZXTools'],
       author="Vladimir Berezenko",
       author_email="qmaster@rambler.ru",
